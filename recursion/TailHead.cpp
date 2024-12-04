ba7860c20be96986b7ac3b41a71e79707adb486c
lorenzo <lorenzodonato75@gmail.com>
2024-12-04 16:05:04 -0300

    Head and tail recursion examples

---
+++ recursion/TailHead.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+
+void tailRecursion(int n)
+{
+    if(n > 0)
+    {
+        // first print n
+        printf("First print %d \n", n);
+        // then call the function
+        printf("Then call the function fun(n - 1) \n");
+        tailRecursion(n - 1);
+    }
+}
+
+void headRecursion(int n)
+{
+    if(n > 0)
+    {
+        // first call the function
+        printf("First call the function fun(n - 1) \n");
+        headRecursion(n - 1);
+        // then print n
+        printf("Then print: %d \n", n);
+    }
+}
+
+int main()
+{
+    int x = 3;
+    
+    printf("Tail Recursion of x = %d : \n", x);
+    tailRecursion(x);
+    printf("\n");
+    printf("Head Recursion of x = %d : \n", x);
+    headRecursion(x);
+
+    return 0;
+}
