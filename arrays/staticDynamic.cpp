f2d1322b0085c135f55d26f764ea4230af2a3926
lorenzo <lorenzodonato75@gmail.com>
2024-12-05 18:41:48 -0300

    first cpp file arrays/staticDynamic.cpp

---
+++ arrays/staticDynamic.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A[5]; // static
+    int n;
+    printf("Enter size of aray: ");
+    cin >> n;
+    int B[n]; // stack only
+
+
+
+    return 0;
+}
