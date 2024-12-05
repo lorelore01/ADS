a6cf58ff41e1655260b91de0120654b775e9366e
lorenzo <lorenzodonato75@gmail.com>
2024-12-05 18:43:55 -0300

    memory

--- arrays/staticDynamic.cpp
+++ arrays/staticDynamic.cpp
@@ -4,12 +4,18 @@ using namespace std;
 
 int main()
 {
-    int A[5]; // static
-    int n;
-    printf("Enter size of aray: ");
-    cin >> n;
-    int B[n]; // stack only
+    // int A[5]; // static
+    // int n;
+    // printf("Enter size of aray: ");
+    // cin >> n;
+    // int B[n]; // stack only
 
+    // dynamic
+
+    int A[5];
+    int *p;
+    p = new int[5]; // 'C++' 
+    // (int *) malloc (5 * sizeof(int)) 'C'-lang
 
 
     return 0;
