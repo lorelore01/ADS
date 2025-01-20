e19b31501bd859238e5d28fd5bddcb8e8ebeffeb
lorenzo <lorenzodonato75@gmail.com>
2025-01-20 18:21:43 -0300

    count and sum ll

--- linkedLists/countAndSum.cpp
+++ linkedLists/countAndSum.cpp
@@ -37,15 +37,55 @@ void Display(struct Node *p)
     }
 }
 
+int count(struct Node *p)
+{
+    int l = 0;
+    while (p)
+    {
+        l++;
+        p = p->next;
+    }
+    return l;
+}
+
+int Rcount(struct Node *p)
+{
+    if (p != NULL)
+        return Rcount(p->next) + 1;
+    else
+        return 0;
+}
+
+int sum(struct Node *p)
+{
+    int s = 0;
+
+    while(p != NULL)
+    {
+        s += p->data;
+        p = p->next;
+    }
+
+    return s;
+}
+
+int Rsum(struct Node *p)
+{
+    if(p == NULL)
+        return 0;
+    else
+        return Rsum(p->next) + p->data;
+}
 
 int main()
 {
-    int A[] = {3, 5, 6, 7, 10, 15};
+    int A[] = {3, 5, 6, 7, 10, 15, 8, 12, 29};
 
-    create(A, 5);
+    create(A, 8);
 
     Display(first);
-    
+    printf("\nLength is %d\n", count(first));
+    printf("\nSum is %d\n\n", sum(first));
 
     return 0;
 }
