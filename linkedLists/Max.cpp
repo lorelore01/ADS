9d6bc334030e92a66498b2e64ce8a018c320e829
lorenzo <lorenzodonato75@gmail.com>
2025-01-20 20:07:59 -0300

    linked list search

---
+++ linkedLists/Max.cpp
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = (struct Node *) malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = (struct Node *) malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+
+    }
+}
+
+int count(struct Node *p)
+{
+    int l = 0;
+    while (p)
+    {
+        l++;
+        p = p->next;
+    }
+    return l;
+}
+
+int Rcount(struct Node *p)
+{
+    if (p != NULL)
+        return Rcount(p->next) + 1;
+    else
+        return 0;
+}
+
+int sum(struct Node *p)
+{
+    int s = 0;
+
+    while(p != NULL)
+    {
+        s += p->data;
+        p = p->next;
+    }
+
+    return s;
+}
+
+int Rsum(struct Node *p)
+{
+    if(p == NULL)
+        return 0;
+    else
+        return Rsum(p->next) + p->data;
+}
+
+int Max(struct Node *p)
+{
+    int max = INT_MIN;
+
+    while (p)
+    {
+        if(p->data > max)
+            max = p->data;
+        p = p->next;
+    }
+    return max;
+}
+
+int RMax(struct Node *p)
+{
+    int x = 0;
+
+    if (p == 0)
+        return INT_MIN;
+    x = RMax(p->next);
+    if (x > p->data)
+        return x;
+    else return p->data;
+    
+}
+
+int main()
+{
+    int A[] = {3, 5, 6, 7, 10, 15, 8, 12, 29};
+
+    create(A, 8);
+
+    Display(first);
+    printf("\nLength is %d\n", count(first));
+    printf("\nSum is %d\n\n", sum(first));
+    printf("\nMax is %d\n", RMax(first));
+
+    return 0;
+}
